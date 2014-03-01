cbd2647a5aa54783c18bc02316b730bd1720c007
Adi Singh <adisin8@gmail.com>
2014-03-01 10:34:09 -0800

    Optimized code for better CV emulation.

--- cv_sketch/cv_sketch.ino
+++ cv_sketch/cv_sketch.ino
@@ -7,12 +7,14 @@
  
 uint8_t buf[8] = {}; 	/* Keyboard report buffer */
  
-#define PIN_COPY 6
-#define PIN_PASTE 7
+#define PIN_COPY 7
+#define PIN_PASTE 8
+#define PIN_C_LED 9
+#define PIN_P_LED 6
  
 int state = 1;
-int c_flag = 0;
-int p_flag = 0;
+boolean c_flag = false;
+boolean p_flag = false;
 
 void setup() 
 {
@@ -20,46 +22,46 @@ void setup()
  
   pinMode(PIN_COPY, INPUT);
   pinMode(PIN_PASTE, INPUT);
+  pinMode(PIN_C_LED, OUTPUT);
+  pinMode(PIN_P_LED, OUTPUT);
   // Enable internal pull-ups
   digitalWrite(PIN_COPY, 1); 
   digitalWrite(PIN_PASTE, 1); 
- 
+  
   delay(200);
 }
  // check if bug was solved because of buf=8 or because of adding last else statement.
 void loop() 
 { 
   state = digitalRead(PIN_COPY);
-  if (state != 1 && c_flag == 0) {
-    c_flag = 1;
+  if (state != 1 && !c_flag) {
+    c_flag = true;
     buf[0] = KEY_LEFT_CTRL; 	// Ctrl
     buf[2] = 6;    // Letter C
-    // buf[2] = 124;	  // Copy key: Less portable
     Serial.write(buf, 8);	// Send keypress
-//    Serial.write("c");
+    digitalWrite(PIN_C_LED, 1);
+ //   Serial.write("\nc");
     releaseKey();
   } else if (state == 1) {
-    c_flag = 0;
-    delay(100);
-  } else {
-    releaseKey();
+    c_flag = false;
+    delay(50);
   }
  
   state = digitalRead(PIN_PASTE);
-  if (state != 1 && p_flag == 0) {
-    p_flag = 1;
+  if (state != 1) {
     buf[0] = KEY_LEFT_CTRL; 	// Ctrl
     buf[2] = 25;    // Letter V
-    // buf[2] = 125;	  // Paste key: Less portable
     Serial.write(buf, 8);	// Send keypress
-//    Serial.write("v");
+    digitalWrite(PIN_P_LED, 1);
+ //   Serial.write("\nv");
     releaseKey();
-  } else if (state == 1) {
-    p_flag = 0;
-    delay(100);
-  } else {
-    releaseKey();
-  }
+    
+    // handles repetitive printing with initial pause
+    if (!p_flag) {
+      p_flag = true;
+      delay(500);
+    }// else delay(50);
+  } else p_flag = false;
 }
  
 void releaseKey() 
@@ -67,5 +69,7 @@ void releaseKey()
   buf[0] = 0;
   buf[2] = 0;
   Serial.write(buf, 8);	// Release key  
-  delay(200);
+  digitalWrite(PIN_C_LED, 0);
+  digitalWrite(PIN_P_LED, 0);
+  delay(100);
 }
